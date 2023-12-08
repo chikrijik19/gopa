4935b5eb4ad45a30ea8f2cccd816bc1d08111c1d
chikrijik19 <145467375+chikrijik19@users.noreply.github.com>
2023-12-08 17:45:23 +0300

    Add files via upload

--- pr.py
+++ pr.py
@@ -34,69 +34,20 @@ def right():
 
 
 
-def left():
-    enemy_name = 'гопники'
+def left(): 
     print('гопникии:')
     print('слышь бабки гони а то в деревянном ящике под землёй окажешься')
     choise = input('воу, ситуация не очень, если идти в бой, то нажми 1, если хочешь предложить им в кости, то нажми 2 ')
     if choise =='1':
-        print('БОЙ НАЧИНАЕТСЯ!')
-        while True:
-            player_attak = name_foce
-            input('нажмите Enter что бы бить ')
-            enemy_hp =- player_attak
-            print(name, 'ударил', enemy_name, 'на', player_attak, 'hp')
-            print('y', enemy_name, 'осталось', enemy_hp, 'жизней')
-            if name_hp <= 0:
-                print(name, 'проиграл')
-                break
-
-            enemy_attak = enemy_foce
-            name_hp =- enemy_attak
-            print(enemy_name, 'ударил', name, 'на', enemy_attak, 'hp')
-            print('y', name, 'осталось', name_hp, 'жизней')
-            if enemy_hp <= 0:
-                print(enemy_name, 'проиграл')
-                break
+        fight(name)
     elif choise == '2':
-        print('гопники:')
-        print('нуу, так тоже можно')
-        if name_money >= 1:
-            print('ИГРА НАЧАЛАСЬ!')
-            print('__________________________________')
-            print('y', name, name_money, 'денег')
-            bet = int(input('сколько ставишь? '))
-            if bet > name_money:
-                print('да нет у тебя столько денег')
-            elif bet < 1:
-                print('ставь нормальную ставку')
-            else:
-                print('ставка игрока', bet)
-                name_bet = randint(1, 6)
-                enemy_bet = randint(1, 6)
-                if name_bet > enemy_bet:
-                    name_money =+ bet * 2
-                    print('число', name, name_bet, 'число', enemy_name, enemy_bet)
-                    print(name, 'выиграл', bet * 2, 'долларов')
-                elif name_bet == enemy_bet:
-                    print('ничья')
-                else:
-                     print('число', name, name_bet, 'число', enemy_name, enemy_bet)
-                     print(name, 'проиграл', bet , 'долларов')
-        else:
-            print('так стоп, у тебя нет денег клоун')
-        
-                            
-
-
-        
-
-
+        dice(name, name_money, enemy_name='гопники')            
 
 
 
 def center():
     print('опа деньги')
+    name_money = (name_money + 5)
 
 
 
@@ -130,6 +81,73 @@ def stall():
         print('ну пока')
     else:
         print('немного не понял, повтори')
+
+
+
+def fight(
+        name,
+        name_level=1,
+        name_hp=1 + name_level,
+        name_foce=randint(1, 5) * name_level,
+        enemy_name='гопники',
+        enemy_hp=12 + enemy_level,
+        enemy_level=1,
+        enemy_foce=randint(2, 7) * enemy_level
+):
+        print('БОЙ НАЧИНАЕТСЯ!')
+        while True:
+            player_attak = name_foce
+            input('-------нажмите Enter что бы бить-------')
+            enemy_hp -= player_attak
+            print(name, 'ударил', enemy_name, 'на', player_attak, 'hp')
+            print('y', enemy_name, 'осталось', enemy_hp, 'жизней')
+            if name_hp <= 0:
+                print(name, 'проиграл')
+                break
+
+            enemy_attak = enemy_foce
+            name_hp -= enemy_attak
+            print(enemy_name, 'ударил', name, 'на', enemy_attak, 'hp')
+            print('y', name, 'осталось', name_hp, 'жизней')
+            if enemy_hp <= 0:
+                print(enemy_name, 'проиграл')
+                break
+
+
+
+def dice(name, name_money, enemy_name='гопники'):
+    print('гопники:')
+    print('нуу, так тоже можно')
+    if name_money >= 1:
+        print('ИГРА НАЧАЛАСЬ!')
+        print('__________________________________')
+        print('y', name, name_money, 'денег')
+        bet = int(input('сколько ставишь? '))
+        if bet > name_money:
+            print('да нет у тебя столько денег')
+        elif bet < 1:
+            print('ставь нормальную ставку')
+        else:
+            print('ставка игрока', bet)
+            name_bet = randint(1, 6)
+            enemy_bet = randint(1, 6)
+            if name_bet > enemy_bet:
+                name_money += bet * 2
+                print('число', name, name_bet, 'число', enemy_name, enemy_bet)
+                print(name, 'выиграл', bet * 2, 'долларов')
+                print('y', name, name_money, 'долларов')
+            elif name_bet == enemy_bet:
+                print('ничья')
+            else:
+                name_money -= bet
+                print('число', name, name_bet, 'число', enemy_name, enemy_bet)
+                print(name, 'проиграл', bet , 'долларов')
+                print('y', name, name_money, 'долларов')
+    else:
+        print('так стоп, у тебя нет денег клоун')
+
+
+    
         
 
 
