9784d117ce1c45fb6f2642fa519b37d612a49489
chikrijik19 <145467375+chikrijik19@users.noreply.github.com>
2024-03-15 19:46:46 +0300

    Add files via upload

--- idk.py
+++ idk.py
@@ -69,14 +69,16 @@ def get_players() -> list[dict]:
     player_1 = {
         'имя': 'Вася',
         'карты': [],
-        'человек': True,
-        'сумма': 0
+        'человек': False,
+        'сумма': 0,
+        'предел': 17
     }
     player_2 = {
         'имя': 'Яблоко',
         'карты': [],
         'человек': False,
-        'сумма': 0
+        'сумма': 0,
+        'предел': 16
     }
     return [player_1, player_2]
 
@@ -100,49 +102,70 @@ def calculate_total_values(player: dict) -> None:
     player['сумма'] = total
 
 
-deck = get_deck()
-shuffle(deck)
-players = get_players()
-cards_per_turn = len(deck) // len(players)
-deal_cards(2)
-
-for player in players:
-    while True:
-        system('cls')
-        show_cards()
-        calculate_total_values(player)
-        print('сумма очков:', player['сумма'])
-        if player['человек']:
-            player_option = input('Взять карту из колоды (y/n)? ')
+def show_statistics():
+    for player in players:
+        if player['сумма'] > 21:
+            print(player['имя'], 'набрал', player['сумма'], 'очков......перебор!')
         else:
-            if player['сумма'] < 17:
-                player_option = 'y'
-            else:
-                player_option = 'n'
-        if player_option.lower() == 'y':
-            if len(player['карты']) < cards_per_turn:
-                player['карты'].append(deck.pop())
-            else:
-                print('Невозможно взять ещё карту!')
-                break
-        else:
-            break
-    input('Enter - следующий игрок ')
+            print(player['имя'], 'набрал', player['сумма'], 'очков')
 
 
-# у всех перебор
-# у всех равны
-#
-players_total_values = [player['сумма'] for player in players if player['сумма'] < 22]
-if players_total_values:
-    max_value = max(players_total_values)
-
-for player in players:
-    if player['сумма'] > 21:
-        print(player['имя'], 'набрал', player['сумма'], 'очков......перебор!')
-    elif player['сумма'] == max_value:
-        print(player['имя'], 'набрал', player['сумма'], 'очков......победил!')
+def show_result() -> str:
+    totle_values = [player['сумма'] for player in players]
+    
+    if totle_values.count(totle_values[0]) == len(totle_values):
+        print('Ничья') # у всех одинаковое кол-во очков
+        return 'Ничья'
     else:
-        print(player['имя'], 'набрал', player['сумма'], 'очков')
+        totle_values = [player['сумма'] for player in players if player['сумма'] < 22]
+        for player in players:
+            if player['сумма'] == max(totle_values):
+                print('Победил', player['имя'])
+                return player['имя']
+
+statistics = dict()
+
+for i in range(107):
+    deck = get_deck()
+    shuffle(deck)
+    players = get_players()
+    cards_per_turn = len(deck) // len(players)
+    deal_cards(2)
+
+    for player in players:
+        while True:
+            system('cls')
+            show_cards()
+            calculate_total_values(player)
+            print('сумма очков:', player['сумма'])
+            if player['человек']:
+                player_option = input('Взять карту из колоды (y/n)? ')
+            else:
+                if player['сумма'] < 17:
+                    player_option = 'y'
+                else:
+                    player_option = 'n'
+            if player_option.lower() == 'y':
+                if len(player['карты']) < cards_per_turn:
+                    player['карты'].append(deck.pop())
+                else:
+                    print('Невозможно взять ещё карту!')
+                    break
+            else:
+                break
+    
 
 
+system('cls')
+# show_statistics()
+winner = show_result()
+
+'''
+написать на перерыве маме что бы написала заявление об переходе в другую групу
+'''
+if winner not in statistics:
+    statistics[winner] = 1
+else:
+    statistics[winner] += 1
+
+print(statistics)
